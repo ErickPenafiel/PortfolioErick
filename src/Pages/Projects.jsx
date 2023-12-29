6dd36c1e78e777eb69e1556934cf9fa72d22d187
Peñafiel Erick <penafiel.erick.martin@gmail.com>
2023-12-29 18:14:31 -0400

    Add projects

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -1,5 +1,3 @@
-import { CssIcon, HtmlIcon, JavaScriptIcon, LaravelIcon } from '../components/Icons'
-import { GithubIcon, WebIcon } from '../components/NavbarIcons'
 import { Project } from '../components/Project'
 import { projects } from '../mocks/projects'
 import './Projects.css'
@@ -8,53 +6,13 @@ export function Projects () {
   return (
     <main id='projects' className='projects'>
         <div className='list-projects'>
-
             {
-                projects.map(({ video, title, description, web, repository, id }) => {
+                projects.map(({ image, title, description, web, repository, id }) => {
                   return (
-                    <Project video={video} title={title} description={description} web={web} repository={repository} key={id} />
+                    <Project image={image} title={title} description={description} web={web} repository={repository} key={id} />
                   )
                 })
             }
-
-            <div className="project">
-                <img src="https://images.pexels.com/photos/19478656/pexels-photo-19478656/free-photo-of-nieve-hombre-pareja-mujer.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" />
-                <div className='information-project'>
-                    <p className='project-title'>Administrador Seguridad Industrial</p>
-                    <p className='project-tecnology'><HtmlIcon color='white' width={'20px'} /> <CssIcon color='white' width={'20px'} /> <JavaScriptIcon color='white' width={'20px'} /> <LaravelIcon color='white' width={'20px'} /></p>
-                    <p className='project-description'>El proyecto consiste en implementar un control de todo en cuanto a seguridad en una industria.</p>
-                    <div style={{ textAlign: 'right', paddingRight: '1rem', display: 'flex', justifyContent: 'flex-end', gap: '0.5rem' }}>
-                        <a href='' className='btn-project'><WebIcon color='white' width='15px' />Web</a>
-                        <a href='' className='btn-project'><GithubIcon color='white' width='15px' />Repositorio</a>
-                    </div>
-                </div>
-            </div>
-
-            <div className="project">
-                <img src="https://images.pexels.com/photos/19488566/pexels-photo-19488566/free-photo-of-kioto.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" />
-                <div className='information-project'>
-                    <p className='project-title'>Administrador Seguridad Industrial</p>
-                    <p className='project-tecnology'><HtmlIcon color='white' width={'20px'} /> <CssIcon color='white' width={'20px'} /> <JavaScriptIcon color='white' width={'20px'} /> <LaravelIcon color='white' width={'20px'} /></p>
-                    <p className='project-description'>El proyecto consiste en implementar un control de todo en cuanto a seguridad en una industria.</p>
-                    <div style={{ textAlign: 'right', paddingRight: '1rem', display: 'flex', justifyContent: 'flex-end', gap: '0.5rem' }}>
-                        <a href='' className='btn-project'><WebIcon color='white' width='15px' />Web</a>
-                        <a href='' className='btn-project'><GithubIcon color='white' width='15px' />Repositorio</a>
-                    </div>
-                </div>
-            </div>
-
-            <div className="project">
-                <img src="https://images.pexels.com/photos/19237996/pexels-photo-19237996/free-photo-of-carretera-bosque-arboles-paisaje.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" />
-                <div className='information-project'>
-                    <p className='project-title'>Administrador Seguridad Industrial</p>
-                    <p className='project-tecnology'><HtmlIcon color='white' width={'20px'} /> <CssIcon color='white' width={'20px'} /> <JavaScriptIcon color='white' width={'20px'} /> <LaravelIcon color='white' width={'20px'} /></p>
-                    <p className='project-description'>El proyecto consiste en implementar un control de todo en cuanto a seguridad en una industria.</p>
-                    <div style={{ textAlign: 'right', paddingRight: '1rem', display: 'flex', justifyContent: 'flex-end', gap: '0.5rem' }}>
-                        <a href='' className='btn-project'><WebIcon color='white' width='15px' />Web</a>
-                        <a href='' className='btn-project'><GithubIcon color='white' width='15px' />Repositorio</a>
-                    </div>
-                </div>
-            </div>
         </div>
     </main>
   )
--- src/mocks/projects.jsx
+++ src/mocks/projects.jsx
@@ -4,7 +4,7 @@ export const projects = [
     title: 'Tic Tac Toe',
     tecnologies: ['HTML', 'CSS', 'Javascript', 'React'],
     description: 'Tic tac toe game made with React',
-    video: 'src/assets/video/TicTacToe.webm',
+    image: 'src/assets/images/TicTacToe.webp',
     web: 'https://tictactoe-penafiel.netlify.app/',
     repository: 'https://github.com/ErickPenafiel/ReactCurso/tree/main/Proyectos/02-tres-en-raya'
   },
@@ -13,7 +13,7 @@ export const projects = [
     title: 'Mouse Follower',
     tecnologies: ['HTML', 'CSS', 'Javascript', 'React'],
     description: 'A mouse follower made with React',
-    video: 'src/assets/video/MouseFollower.webm',
+    image: 'src/assets/images/MouseFollower.webp',
     web: 'https://shimmering-sunflower-0f2431.netlify.app/',
     repository: 'https://github.com/ErickPenafiel/ReactCurso/tree/main/Proyectos/03-mouse-follower'
   },
@@ -22,7 +22,7 @@ export const projects = [
     title: 'Search Movies',
     tecnologies: ['HTML', 'CSS', 'Javascript', 'React'],
     description: 'Search movies made with React',
-    video: 'src/assets/video/SearchMov.webm',
+    image: 'src/assets/images/SearchMovies.webp',
     web: 'https://playful-bavarois-db52d9.netlify.app/',
     repository: 'https://github.com/ErickPenafiel/ReactCurso/tree/main/Proyectos/05-react-buscador-peliculas'
   },
@@ -31,7 +31,7 @@ export const projects = [
     title: 'Cart Shop',
     tecnologies: ['HTML', 'CSS', 'Javascript', 'React'],
     description: 'Cart shop made with React',
-    video: 'src/assets/video/CartShop.webm',
+    image: 'src/assets/images/CartShop.webp',
     web: 'https://exquisite-phoenix-7b195c.netlify.app/',
     repository: 'https://github.com/ErickPenafiel/ReactCurso/tree/main/Proyectos/06-shopping-cart'
   },
@@ -40,7 +40,24 @@ export const projects = [
     title: 'Counting Objects',
     tecnologies: ['Python', 'Pytorch'],
     description: 'Counting objects made with Pytorch YOLOv8 and DeepSort',
-    video: 'src/assets/video/CountingObjects.webm',
+    image: 'src/assets/images/CountingObjects.webp',
     repository: 'https://github.com/ErickPenafiel/TrackingCountingObjects'
+  },
+  {
+    id: crypto.randomUUID(),
+    title: 'Weed Detection in Agricultural Fields',
+    tecnologies: ['Python', 'Pytorch'],
+    description: 'Weed detection in agricultural fields made with Pytorch YOLOv8 and DeepSort',
+    image: 'src/assets/images/WeedDetection.webp',
+    repository: 'https://github.com/ErickPenafiel/DeteccionMalezas'
+  },
+  {
+    id: crypto.randomUUID(),
+    title: 'Crud - React with Redux',
+    tecnologies: ['HTML', 'CSS', 'Typescript', 'React'],
+    description: 'Crud made with React and Redux',
+    image: 'src/assets/images/CrudReactRedux.webp',
+    web: 'https://superb-bunny-36fa1f.netlify.app/',
+    repository: 'https://github.com/ErickPenafiel/CrudReactRedux'
   }
 ]
