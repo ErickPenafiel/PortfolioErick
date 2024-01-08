409bf2481ed9820a90a9bb76921fc1ff4c96d0f2
Peñafiel Erick <penafiel.erick.martin@gmail.com>
2024-01-08 14:48:18 -0400

    Fixed Language

--- src/components/ButtonNav.jsx
+++ src/components/ButtonNav.jsx
@@ -1,12 +1,18 @@
 import './ButtonNav.css'
 import { Link } from './Link'
 
-export function ButtonNav ({ icon, idSection, link, label, onMouseEnter, onMouseLeave }) {
+export function ButtonNav ({ icon, idSection, link, label, onMouseEnter, onMouseLeave, onClick }) {
   if (link) {
     return (
       <a id={label} className="button-nav" href={link} target='_blank' rel="noreferrer" onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
         {icon}
       </a>
+    )
+  } else if (idSection === 'contact') {
+    return (
+      <button id={label} className="button-nav prueba" onMouseLeave={onMouseLeave} onMouseEnter={onMouseEnter} onClick={onClick}>
+        {icon}
+      </button>
     )
   } else {
     return (
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,8 @@ export function Navbar () {
     contact: false,
     linkedin: false,
     github: false,
-    email: false
+    email: false,
+    language: false
   })
 
   const handleMouseEnter = (id) => {
@@ -26,19 +27,34 @@ export function Navbar () {
     setTooltipVisibility((prevState) => ({ ...prevState, [id]: false }))
   }
 
+  const handleClick = () => {
+    console.log('click')
+    setTooltipVisibility((prevState) => ({ ...prevState, contact: !prevState.contact, language: !prevState.language }))
+  }
+
+  const handleChangeLanguage = (language) => {
+    i18n.changeLanguage(language)
+    setTooltipVisibility((prevState) => ({ ...prevState, contact: false, language: false }))
+  }
+
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
       <nav className='nav'>
         <ButtonNav icon={<HomeIcon width={'24px'} />} idSection='home' onMouseEnter={() => handleMouseEnter('home')} onMouseLeave={() => handleMouseLeave('home')} />
         <ButtonNav icon={<SkillsIcon width={'24px'} />} idSection='skills' onMouseEnter={() => handleMouseEnter('skills')} onMouseLeave={() => handleMouseLeave('skills')} />
         <ButtonNav icon={<ProjectsIcon width={'24px'} />} idSection='projects' onMouseEnter={() => handleMouseEnter('projects')} onMouseLeave={() => handleMouseLeave('projects')} />
-        <ButtonNav icon={<LanguageIcon width={'24px'} />} idSection='contact' onMouseEnter={() => handleMouseEnter('contact')} onMouseLeave={() => handleMouseLeave('contact')} />
+        {/* <ButtonNav icon={<LanguageIcon width={'24px'} />} idSection='contact' onMouseEnter={() => handleMouseEnter('contact')} onMouseLeave={() => handleMouseLeave('contact')} /> */}
+        <ButtonNav icon={<LanguageIcon width={'24px'} />}
+          idSection='contact'
+          onMouseEnter={() => handleMouseEnter('contact')}
+          onMouseLeave={() => handleMouseLeave('contact')}
+          onClick={handleClick} />
         <span style={{ backgroundColor: 'white', padding: '5px 0', opacity: '0.5', borderRadius: '5px', margin: '0 1rem', userSelect: 'none' }}>|</span>
         <ButtonNav icon={<LinkedInIcon width={'24px'} />} link='about' onMouseEnter={() => handleMouseEnter('linkedin')} onMouseLeave={() => handleMouseLeave('linkedin')} />
         <ButtonNav icon={<GithubIcon width={'24px'} />} link='https://github.com/ErickPenafiel' onMouseEnter={() => handleMouseEnter('github')} onMouseLeave={() => handleMouseLeave('github')} />
         <ButtonNav icon={<MailIcon width={'24px'} />} link='mailto:penafiel.erick.martin@gmail.com' onMouseEnter={() => handleMouseEnter('email')} onMouseLeave={() => handleMouseLeave('email')} />
       </nav>
-      <div className='tooltips'>
+      <div className={'tooltips'}>
         <span className={`tooltip 
           ${i18n.language === 'es' ? 'home-tooltip-es' : 'home-tooltip'}
           ${tooltipVisibility.home ? 'visible' : ''}`}>{t('navbar.home')}</span>
@@ -48,18 +64,19 @@ export function Navbar () {
         <span className={`tooltip 
           ${i18n.language === 'es' ? 'projects-tooltip-es' : 'projects-tooltip'}
           ${tooltipVisibility.projects ? 'visible' : ''}`}>{t('navbar.projects')}</span>
-        {/* <span className={`tooltip
+        <span className={`tooltip
           ${i18n.language === 'es' ? 'contact-tooltip-es' : 'contact-tooltip'}
-          ${tooltipVisibility.contact ? 'visible' : ''}`}>{t('navbar.language')}</span> */}
-        <div className={`languages ${tooltipVisibility.contact ? 'visible' : ''}`}>
-          <button className="btn-languages">
+          ${tooltipVisibility.contact ? 'visible' : ''}`}>{t('navbar.language')}</span>
+        <div className={`
+          ${i18n.language === 'es' ? 'languages-es' : 'languages'} 
+          ${tooltipVisibility.language ? 'visible' : ''}`}>
+          <button className="btn-languages" onClick={() => handleChangeLanguage('es')}>
             Español
           </button>
-          <button className="btn-languages">
+          <button className="btn-languages" onClick={() => handleChangeLanguage('en')}>
             English
           </button>
         </div>
-        <span className='tooltip'>|</span>
         <span className={`tooltip 
           ${i18n.language === 'es' ? 'linkedin-tooltip-es' : 'linkedin-tooltip'}
           ${tooltipVisibility.linkedin ? 'visible' : ''}`}>LinkedIn</span>
